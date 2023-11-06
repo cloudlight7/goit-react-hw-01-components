b1095db5c5a9e38f74becff63bdc27fd97a81dfd
cloudlight7 <cloudlight7@gmail.com>
2023-11-06 16:45:51 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export const App = () => {
     
     <FriendList friends={friends} />
     
-    <TransactionHistory items={transactions} />;
+    <TransactionHistory items={transactions} />
     </div>
   );
 };
